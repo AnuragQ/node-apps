fd8c8bf13b189c6103961a5c5757fcd1446afbd7
anurag <anurags153@gmail.com>
2019-03-18 15:22:08 +0530

    third Party modules

--- require/app.js
+++ require/app.js
@@ -3,10 +3,17 @@ console.log('starting app.js');
 
 const fs = require('fs');
 
-const user = require('user-info');
-console.log(user());
-fs.appendFile('greetings.txt',`hello ${user().username}!`,function(err){
-    if(err){
-        console.log('unable to open file');
-    }
-});
+const notes=require('./notes');
+const _=require('lodash');
+
+// const user = require('user-info');
+// console.log(user());
+// fs.appendFile('greetings.txt',`hello ${user().username}!`,function(err){
+//     if(err){
+//         console.log('unable to open file');
+//     }
+// });
+// console.log(notes.add(8,-3));
+
+var filArrary=_.uniq(['anurag','anurag',2,3,4,2,5,6]);
+console.log(filArrary);
